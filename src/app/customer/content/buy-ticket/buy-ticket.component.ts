b056b92a77edac24cb7394f6e4998132570373af
DESKTOP-4OKI60B\trid2 <trid2300@gmail.com>
2022-08-29 15:35:39 +0700

    c

--- src/app/customer/content/buy-ticket/buy-ticket.component.ts
+++ src/app/customer/content/buy-ticket/buy-ticket.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./buy-ticket.component.css']
 })
 export class BuyTicketComponent implements OnInit {
-
+  scroll: number = 0
   constructor() { }
 
   ngOnInit(): void {
+    window.addEventListener('scroll', () => {
+      if (document.documentElement.scrollTop > 300) {
+        document.getElementById('scroll1')?.classList.add('scroll-show')
+        document.getElementById('scroll2')?.classList.add('scroll-show')
+        document.getElementById('scroll3')?.classList.add('scroll-show')
+      }
+    })
   }
 
 }
