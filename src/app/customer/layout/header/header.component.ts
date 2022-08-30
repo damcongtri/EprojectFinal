937943059c9d21504cd73d6e7d4cd9ab0901e041
DESKTOP-4OKI60B\trid2 <trid2300@gmail.com>
2022-08-30 17:27:13 +0700

    f

--- src/app/customer/layout/header/header.component.ts
+++ src/app/customer/layout/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  check: boolean = false
   constructor() { }
 
   ngOnInit(): void {
@@ -17,6 +17,23 @@ export class HeaderComponent implements OnInit {
         document.querySelector('.header')?.classList.remove('bg-header')
       }
     })
-  }
+    document.querySelector('.nav-mb2')?.addEventListener('click', (e: any) => {
+      if (document.querySelector('.nav-mb2')?.contains(e.target)) {
+        document.querySelector('.nav-mobile')?.classList.remove("show-nav")
+        this.check = false
+      }
 
+    })
+
+  }
+  shownav() {
+    if (this.check) {
+      document.querySelector('.nav-mobile')?.classList.remove("show-nav")
+      this.check = false
+    } else {
+      document.querySelector('.nav-mobile')?.classList.add("show-nav")
+      this.check = true
+    }
+
+  }
 }
