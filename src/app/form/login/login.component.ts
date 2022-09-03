d6918b50e924447da29772a3c6bf3e7e5228b413
DESKTOP-4OKI60B\trid2 <trid2300@gmail.com>
2022-09-03 15:48:33 +0700

    v

--- src/app/form/login/login.component.ts
+++ src/app/form/login/login.component.ts
@@ -7,40 +7,39 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  
+
   constructor() { }
 
-  FormValidate:any = new FormGroup ({
-    email:new FormControl('', [Validators.required , Validators.email]),
-    password: new FormControl('',[Validators.required, Validators.minLength(8)]),
-    ConfirmPassword: new FormControl('',[Validators.required, Validators.minLength(8)])
+  FormValidate: any = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
   })
 
-  get f():any{
+  get f(): any {
     return this.FormValidate.controls;
   }
 
-  onSubmit(){
-    if(!this.FormValidate.invalid){
+  onSubmit() {
+    if (!this.FormValidate.invalid) {
 
     }
   }
-  
+
   ngOnInit(): void {
-    var pwd:any = document.getElementById("pwd");
+    var pwd: any = document.getElementById("pwd");
     var eye = document.getElementById("eye");
 
     eye?.addEventListener("click", togglePass);
 
-    function togglePass(){
+    function togglePass() {
       eye?.classList.toggle("active");
-      pwd.type ==  "password" ? (pwd.type = "text") :
-      (pwd.type = "password");
+      pwd.type == "password" ? (pwd.type = "text") :
+        (pwd.type = "password");
     }
 
   }
 
-  
- 
+
+
 
 }
